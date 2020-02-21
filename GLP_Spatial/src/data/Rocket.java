61254b881d6a115624934b11c81897c987e4e38c
Hike95 <60402524+Hike95@users.noreply.github.com>
2020-02-21 12:53:24 +0100

    Add files via upload

--- GLP_Spatial/src/data/Rocket.java
+++ GLP_Spatial/src/data/Rocket.java
@@ -1,11 +1,27 @@
-package data;
+package glp;
 
 public class Rocket {
-	private float weight ;
-	private float speed ;
-	private float propulsionPower ;
-	private Point rocketLocation ;
 
+	
+	private int weight ;
+	private int speed ;
+	private int power ;
+	private Point rocketLocation ;
+	public Rocket(int weight/*,int speed*/,int power) {
+		this.weight=weight;
+		/*this.speed=speed;*/
+		this.power= power;	
+			// TODO Auto-generated constructor stub
+		
+		// TODO Auto-generated constructor stub
+	}
+	
+	public String toString() {
+		
+		
+	return "weight="+weight+"power="+power+"";	
+		
+	}
 	public void drawRocket (Point rocketLocation) {
 		
 	}
