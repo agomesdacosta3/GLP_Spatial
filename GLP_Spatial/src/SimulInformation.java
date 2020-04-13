d8521661d6c5c8da5d63fcf0b45c7c1a9ae91c1d
Hike95 <60402524+Hike95@users.noreply.github.com>
2020-04-13 16:00:12 +0200

    Add files via upload

--- GLP_Spatial/src/SimulInformation.java
+++ GLP_Spatial/src/SimulInformation.java
@@ -3,7 +3,8 @@ package glp1;
 public class SimulInformation {
 
 	
-	public static final int SIMULATION_DURATION = 2000;
+	public static final int SIMULATION_MARS_DURATION = 2000000000;
+	public static final int SIMULATION_MARS_SPEED = 100;
 	public static final int SIMULATION_SPEED = 100;	
 	public static final int SIMULATION_EARTH_DURATION = 200000;
 	public static final int SIMULATION_EARTH_SPEED = 1;	
